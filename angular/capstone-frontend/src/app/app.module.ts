4e8f2d7a6a3b9714e0270b09adec6f1833ec4c31
James <jravila08@gmail.com>
2020-02-07 20:10:58 -0700

    create rough screens for loginand create account

--- angular/capstone-frontend/src/app/app.module.ts
+++ angular/capstone-frontend/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { MatSidenav, MatSidenavModule, MatButtonModule } from '@angular/material'
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSidenav, MatSidenavModule, MatButtonModule, MatCardModule, MatInputModule, MatFormFieldModule, MatDividerModule, MatSelect, MatSelectModule } from '@angular/material'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 const appRoutes: Routes = [
   {
@@ -16,19 +19,35 @@ const appRoutes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'createAccount',
+    component: CreateAccountComponent
   }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    LoginComponent,
+    CreateAccountComponent
   ],
   imports: [
     BrowserModule,
     MatSidenavModule,
     MatButtonModule,
     BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDividerModule,
+    MatSelectModule,
+    FlexLayoutModule,
     RouterModule.forRoot(appRoutes),
   ],
   providers: [],
